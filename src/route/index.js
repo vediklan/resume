ef28b068c55778f2edb9fd65b5bf4a3466f780d8
Vyacheslav <vediklan@gmail.com>
2023-05-19 03:32:17 +0300

    end task 8

--- src/route/index.js
+++ src/route/index.js
@@ -3044,7 +3044,7 @@ router.get('/shopcatalog', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('shopcatalog', {
-    // layout: 'shop',
+    layout: 'shop',
     navigation: {
       links: [
         {
